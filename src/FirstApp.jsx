2022a26e6d2b386f761c742882e60ab9b1a4f4b1
anec-97 <anechdz22@gmail.com>
2024-10-31 15:51:34 -0600

    introduction to a react

---
+++ src/FirstApp.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types"
+
+
+
+export const FirstApp = ({title, subTitle, name }) => {
+
+   
+    //console.log(props);
+    return (
+        <>
+            <h1>{title}</h1>
+            {/* <code> {JSON.stringify(newMessage)}</code>*/}
+            <p> {subTitle}</p>
+            <p>{name}</p>
+        </>
+        
+    )
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string,
+    name: PropTypes.string
+}
+
+FirstApp.defaultProps = {
+    title: 'No hay titulo',
+    subTitle: 'No hay subtitulo',
+    name: 'alex'
+}
---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,12 @@
+//  function App() {
+//     return (<h1>Hello world!!</h1>);
+// }
+
+// export default App
+
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  )
+}
